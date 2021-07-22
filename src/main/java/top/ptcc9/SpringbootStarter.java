e8aaf2ddc9fde8d31fbd505621057237627671f0
HE LONG CAN <15362748867@163.com>
2021-07-22 16:28:34 +0800

    启动器

---
+++ src/main/java/top/ptcc9/SpringbootStarter.java
@@ -0,0 +1,16 @@
+package top.ptcc9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author HE LONG CAN
+ * @Description 启动器
+ * @Date 2021-07-22 16:27:37
+ */
+@SpringBootApplication
+public class SpringbootStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStarter.class,args);
+    }
+}
